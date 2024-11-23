47eb51184e7bd898ada2ad1388c896de3a506312
ruslan <raslman1999@yandex.ru>
2024-11-24 00:53:34 +0300

    Добавил фильтры в Чатам, Событиям, Евентам

--- activities/urls.py
+++ activities/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import ActivityViewSet
 
 urlpatterns = [
-    path('activity/<int:telegram_id>/', ActivityViewSet.as_view({'get': 'get'})),
+    path('activity/<int:telegram_id>/', ActivityViewSet.as_view({'get': 'get', 'post': 'post', 'get_user_activity': 'get_user_activity'})),
 ]
--- activities/views.py
+++ activities/views.py
@@ -8,14 +8,30 @@ from users.models import User
 from biscenters.models import Biscenter
 from django.forms import model_to_dict
 import datetime
-from django.db.models import Q
+from django.db.models import Q, Count, F
 
 from rest_framework.viewsets import ViewSet
 
+
 class ActivityViewSet(ViewSet):
-    http_method_names = ['get', 'post', 'head']
+    http_method_names = ['get', 'post']
 
     def get(self, request, telegram_id: int, *args, **kwargs):
-        user_data = User.objects.filter(telegram_id=telegram_id).values_list('biscenter','id','town')[0]
-        activity = Activity.objects.exlude(user=user_data[1]).filter(date_start__gte=datetime.date.today()).filter(Q(biscenter=user_data[0]) | Q(biscenter=None)).values().filter(Q(town=user_data[2]) | Q(town_filter=None)).values()
+        user_data = User.objects.filter(telegram_id=telegram_id).values_list('biscenter', 'id', 'town')[0]
+ #.filter(Q(biscenter=user_data[0]) | Q(biscenter=None))
+        activity = Activity.objects.exclude(user=user_data[1]).filter(date_start__gte=datetime.date.today()).filter(
+            Q(town=user_data[2]) | Q(town_filter=None)).annotate(user_count=Count('user')).filter(users_max_count__gte=F('user_count')).values('id', 'name', 'town', 'date_start', 'user_count', 'users_max_count')
+        return Response({'get': list(activity)})
+
+    def post(self, request, telegram_id: int, *args, **kwargs):
+        activity = Activity.objects.get(id=self.request.POST['activity_id'])
+        user = User.objects.get(telegram_id=telegram_id)
+        activity.user.add(user)
+        activity.save()
         return Response({'post': model_to_dict(activity)})
+
+    def get_user_activity(self, telegram_id: int):
+        user_data = User.objects.filter(telegram_id=telegram_id).values_list('biscenter', 'id', 'town')[0]
+        activity = Activity.objects.filter(user=user_data[1]).filter(date_start__gte=datetime.date.today()).values('id', 'name', 'town', 'date_start')
+        return Response({'get': list(activity)})
+
